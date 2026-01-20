c33bf0369d45687555884a7e97fb819c09ffca2b
ameer <ameerfaisal.adanan@fiuu.com>
2026-01-20 23:14:44 +0800

    added jsdocs

--- src/utils/hashUtils.js
+++ src/utils/hashUtils.js
@@ -1,5 +1,12 @@
 import CryptoJS from 'crypto-js';
 
+/**
+ * Computes the hash of a given input using the specified algorithm.
+ * 
+ * @param {string} input - The input string to hash.
+ * @param {string} algorithm - The hashing algorithm to use ('md5', 'sha1', 'sha256', 'sha512').
+ * @returns {string} The computed hash as a hexadecimal string, or an empty string if the algorithm is not supported.
+ */
 export function computeHash(input, algorithm) {
   switch (algorithm) {
     case 'md5':
@@ -15,6 +22,14 @@ export function computeHash(input, algorithm) {
   }
 }
 
+/**
+ * Computes the HMAC (Hash-based Message Authentication Code) of a given input using a secret and algorithm.
+ * 
+ * @param {string} input - The input string to hash.
+ * @param {string} secret - The secret key for the HMAC.
+ * @param {string} algorithm - The hashing algorithm to use ('md5', 'sha1', 'sha256', 'sha512').
+ * @returns {string} The computed HMAC as a hexadecimal string, or an empty string if the algorithm is not supported.
+ */
 export function computeHmac(input, secret, algorithm) {
   switch (algorithm) {
     case 'md5':
